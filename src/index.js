c021a1a0bfafc2c60aa4d4ead8f1d62dcfd5f570
Tim Keiner <keiner.tim@gmail.com>
2019-01-28 16:25:50 +0100

    added a inex overview page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const {readFile} = require('fs');
+const {join} = require('path');
 const bodyParser = require('body-parser');
 const { getAll, byId, removeTodo, addTodo } = require('./todos');
 
 const app = express();
 
+
 app.use(bodyParser.json());
+
+app.get('', (_, res) => {
+    readFile(join(__dirname, 'index.html'), (e, data) => {
+        res.end(data);
+    })
+})
 
 app.get('/api/todolist', (req, res) => {
     res.json(getAll());
@@ -15,14 +24,15 @@ app.delete('/api/todo/:id', (req, res) => {
     res.json(removeTodo(byId(id)));
 })
 
-app.get('/api/todo/:id', (req, res) => {
-    const {id} = req.param;
-    res.json(byId(id));
+app.get('/api/todo/:id', (req, res) => {    
+    const {id} = req.params;
+    const todo = byId(id);
+    res.json(todo);
 })
 
 app.post('/api/todolist', (req, res) => {
     addTodo(req.body);
-    res.send(req.body.id);
+    res.end("");
 })
 
 app.listen(process.env.PORT || 8080, () => {
